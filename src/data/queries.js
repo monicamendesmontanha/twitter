3bdf1761831435af3cc395c64ce012abae00fff7
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-11 14:56:33 +1000

    Change the order data to show

--- src/data/queries.js
+++ src/data/queries.js
@@ -2,7 +2,9 @@ import gql from 'graphql-tag'
 
 export const TWITTER_FEED_QUERY =  gql`
   query allTweets {
-    allTweets {
+    allTweets (
+      orderBy: createdAt_DESC
+    ) {
       id
       message
       author
